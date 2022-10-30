d261cf90d29a615c1a9413d382698555e7dcecc0
Paulo Silas <paulo3356@gmail.com>
2022-10-30 12:31:45 -0300

    Pagina de Produtos/Serviços functionalidades

--- src/assets/scripts/product/findProducts.js
+++ src/assets/scripts/product/findProducts.js
@@ -1,12 +1,99 @@
+var url = window.location.href;
+var urlObj = new URL(url);
 
-var id = 2
 
-empresas = Empresas.petshops;
+var id = Number(urlObj.searchParams.get("id"))
+
+console.log(id)
+var categoria = 'petshops'
+
+var data = [] ;
+var count = 0;
+
+var logo_user = document.getElementById("logo-user");
+var name_user =  document.getElementById("name-user");
+var ps_descr = document.getElementById("ps-descr");
+var description_ps = document.getElementById("description-ps");
+var header_product_page = document.querySelector('.header-product-page');
+
+var other_products = document.getElementById("other-products");
+var complete_address = document.getElementById("complete-address");
+
 
 addEventListener('load', ()=>{
-    empresas.forEach(empresa => {
-        if(empresa.id === id){
-             GetMap(empresa)
-        }
-    });
+    findCetegory()
+   
 })
+
+
+function findCetegory(){
+        Object.values(Empresas).forEach(element => {
+                findCompany(element)
+         
+            });
+  
+    }
+ 
+
+
+
+function findCompany(data){
+   
+    data.forEach(empresa =>{
+        if(empresa.id === id){
+          updatePage(empresa)
+          GetMap(empresa)
+        }
+    })
+}
+
+
+function updatePage(product){
+    var img_products = '';  
+     
+    header_product_page.style.backgroundImage = `url(${product.imgBanner})`; 
+    header_product_page.style.backgroundSize = 'cover'
+    logo_user.src = `${product.imagens[0]}`;
+    name_user.innerHTML = `${product.nome}`;
+
+    var price = ''
+    if(product.ps_price != undefined && product.ps_price != null && product.ps_price != ''){
+        price = "Preço: " + product.ps_price
+    }
+
+
+
+    var content = ` <div class="descr-images">
+                            <div class="principal-image">
+                                <img src="${product.imagens[0]}">
+                            </div>
+                            <div class="other-images">
+                                <div> <img src="${product.imagens[0]}"></div>
+                                <div> <img src="${product.imagens[0]}"></div>
+                                <div> <img src="${product.imagens[0]}"></div>
+                            </div>
+                        </div>
+                        <div class="description-text">
+                            <div id="ps-title" class="title">${product.nome}</div>
+                            <div id="ps-price">${price}</div>
+                            <div id=""></div>
+                            <div id="ps-descr"> ${product.descricao}  </div>
+                        </div>
+                        `
+ 
+                description_ps.innerHTML = content; 
+
+
+               
+
+               for(var p = 0; p < product.produtos.length; p++){
+                    img_products += `<a href="#"><img class = "other-imgs" src='${product.produtos[p].imagens[0]}' ></a>`
+
+                }
+                               
+               // console.log(product.produtos[p])
+                other_products.innerHTML = img_products
+
+
+                complete_address.innerHTML = `${product.logradouro} ${product.cidade} ${product.estado}` 
+}
