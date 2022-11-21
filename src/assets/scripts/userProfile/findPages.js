c20495100fd1e8e8c9d8f83ef2f9bdf55f950020
Paulo Silas <paulo3356@gmail.com>
2022-11-20 21:01:29 -0300

    update empresa

---
+++ src/assets/scripts/userProfile/findPages.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', ()=>{
+    const pages_data = JSON.parse(localStorage.getItem('pages_data')) 
+    var pages = document.getElementById('pages')
+    var cards= ''
+
+    if(pages_data){
+        for(var i = 0 ; i < pages_data.length; i++){
+             cards += ` <div class="featured-product-item">
+                <div style="background-image: url('${empPro.imagens[0]}');"
+                    class="featured-product-item-image"></div>
+                <p class="title">
+                    ${pages_data[i].nome}
+                </p>
+                <p class="description">
+                    ${pages_data[i].descricao.substr(0,150)} ...
+                </p>
+                <a href="./produto.html?categoria=${pages_data[i].categoria}&id=${pages_data[i].id}&prodserv=${pages_data[i].id}">
+                 <button class="button-bl"> Editar  </button>
+                </a>
+            </div>`
+        }
+    }
+
+    pages.innerHTML = cards
+})
