2918901c1156c2480329e5f6a3124baea7a9830c
Paulo Silas <paulo3356@gmail.com>
2022-11-20 21:06:44 -0300

    update user profile

--- src/assets/scripts/userProfile/findPages.js
+++ src/assets/scripts/userProfile/findPages.js
@@ -6,7 +6,7 @@ window.addEventListener('load', ()=>{
     if(pages_data){
         for(var i = 0 ; i < pages_data.length; i++){
              cards += ` <div class="featured-product-item">
-                <div style="background-image: url('${empPro.imagens[0]}');"
+                <div style="background-image: url('${pages_data[i].imagens[0]}');"
                     class="featured-product-item-image"></div>
                 <p class="title">
                     ${pages_data[i].nome}
