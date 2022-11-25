8892b365dbbcdfeeec0a2f979eaf903a0caaf0de
Paulo Silas <paulo3356@gmail.com>
2022-11-25 20:49:54 -0300

    update edit page

--- src/assets/scripts/create-page/editPages.js
+++ src/assets/scripts/create-page/editPages.js
@@ -10,11 +10,11 @@ function updatePageInputs(){
     var isReservation = document.getElementById("isReservation")
 
     var principal = document.getElementById("principal");
+    var imgPreview = document.getElementById("imgPreview");
     var photo1 = document.getElementById("photo1");
     var photo2 = document.getElementById("photo2");
     var photo3 = document.getElementById("photo3");
-    var photo4 = document.getElementById("photo4");
-
+    
     var rsocial = document.querySelectorAll('.social input');
 
     var product_name = document.getElementById("product_name");
@@ -30,11 +30,19 @@ function updatePageInputs(){
                 if(pages_data[i].id === parseInt(id_produto)){
                 isDeliveries.checked = pages_data[i].domicilio;
                 isReservation.checked = pages_data[i].agendamento;
-            
-                    updateBackground(principal, pages_data[i].banner)
-                    updateBackground(photo1, pages_data[i].imagens[0])
-                    updateBackground(photo2,pages_data[i].imagens[1])
-                    updateBackground(photo3,pages_data[i].imagens[2])
+
+                    updateBackground(principal, pages_data[0])
+                    updateBackground(imgPreview, pages_data[i].banner)
+                    updateBackground(photo1, pages_data[i].imagens[1])
+                    updateBackground(photo2,pages_data[i].imagens[2])
+                    updateBackground(photo3,pages_data[i].imagens[3])
+
+                product_name.value = pages_data[i].nome;
+                product_price.value = pages_data[i].preco;
+                product_description.value = pages_data.descricao
+
+                rsocial[0].value = pages_data.rsocial[0]
+                rsocial[1].value = pages_data.rsocial[1]
             }
             }
         }
