a1945abce8fc4b77504f2bb62bb901256a1a8718
Paulo Silas <paulo3356@gmail.com>
2022-11-24 22:13:44 -0300

    update localstorage

--- src/assets/scripts/database/findData.js
+++ src/assets/scripts/database/findData.js
@@ -303,7 +303,7 @@ function findByLocal(){
         }
 
             for(var i = 0; i < localProdutos.length; i++){
-                console.log(localProdutos)
+                console.log(localProdutos[i])
                 if(localProdutos[i].nome.includes(search) || localProdutos.descricao.includes(search))
                 updateCard(localProdutos[i], empresa)
             }
