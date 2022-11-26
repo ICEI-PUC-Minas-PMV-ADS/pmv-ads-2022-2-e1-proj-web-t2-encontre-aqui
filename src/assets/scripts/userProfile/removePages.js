0630bcfef980112d147a00337325d61af2809ae3
Paulo Silas <paulo3356@gmail.com>
2022-11-25 22:56:19 -0300

    update remover paginas

--- src/assets/scripts/userProfile/removePages.js
+++ src/assets/scripts/userProfile/removePages.js
@@ -1,33 +1,22 @@
 function removePages(page){
     var pages = document.getElementById('pages')
     const pages_data = JSON.parse(localStorage.getItem('pages_data')) 
-    var cards= ''
     var temp = []
-    console.log(temp)
+  
+    if(confirm("Tem certeza que deseja REMOVER está página") == true){
     for(var i = 0 ; i < pages_data.length; i++){
         if(pages_data[i].id != page){
             temp.push(page)
         }
     }
     pages_data = temp
-  
-    for(var i = 0 ; i < pages_data.length; i++){
-        cards += ` <div class="featured-product-item">
-           <div style="background-image: url('${pages_data[i].imagens[0]}');"
-               class="featured-product-item-image"></div>
-           <p class="title">
-               ${pages_data[i].nome}
-           </p>
-           <p class="description">
-               ${pages_data[i].descricao.substr(0,150)} ...
-           </p>
-           <a href="./produto.html?categoria=${pages_data[i].categoria}&id=${pages_data[i].id}&prodserv=${pages_data[i].id}">
-            <button class="button-bl"> Editar  </button> 
-            </a>
-            <button id="removePage" onclick='removePages("${pages_data[i]}")' class="button-bl"> Remover  </button>
-           
-       </div>`
+    localStorage.setItem('pages_data',JSON.stringify(pages_data))
+    window.location.href = "./userProfile.html"
+    }
+   
+   
+    
    }
     
-    pages.innerHTML = cards
-}
+   
+ 
