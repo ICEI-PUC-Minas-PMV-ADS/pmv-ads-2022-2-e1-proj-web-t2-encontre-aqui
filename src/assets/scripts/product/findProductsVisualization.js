102a04418d0726d3e41b2cac286763fc66167b35
Paulo Silas <paulo3356@gmail.com>
2022-12-03 04:03:27 -0300

    update produto

--- src/assets/scripts/product/findProductsVisualization.js
+++ src/assets/scripts/product/findProductsVisualization.js
@@ -162,7 +162,7 @@ function updatePage(empresa){
 
             if(pages_data){
                for(var p = 0; p < pages_data.length; p++){
-                    img_products += `<a href="produtoVisualizacao.html?categoria=${empresa.categoria}&id=${empresa.id}&prodserv=${pages_data[p].id}">
+                    img_products += `<a href="produtoVisualizacao.html?id=${pages_data[p].id}">
                     <div><img class ="other-imgs" src='${pages_data[p].imagens[0]}' >
                         </div><div style="width:150px" class="opened">${pages_data[p].nome}</div></a>`
 
