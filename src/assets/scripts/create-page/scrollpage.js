2c5127dcd945c7200dbb6af01798a00ab0d8cb76
Paulo Silas <paulo3356@gmail.com>
2022-12-05 04:05:55 -0300

    validação de criação

--- src/assets/scripts/create-page/scrollpage.js
+++ src/assets/scripts/create-page/scrollpage.js
@@ -1,14 +1,6 @@
 const sections = document.querySelectorAll('.section')
 const upPage = document.getElementById('upPage')
 const downPage = document.getElementById('downPage')
-var product_name = document.getElementById("product_name");
-var product_price = document.getElementById("product_price");
-var product_description = document.getElementById("product_description");
-var url_img_pr = document.getElementById("url_img_pr");
-var url_img1 = document.getElementById("url_img1");
-var url_img2 = document.getElementById("url_img2");
-var url_img3 = document.getElementById("url_img3");
-var url_img4 = document.getElementById("url_img4");
 
 var previous, next;
 var selected = 0; 
@@ -18,12 +10,6 @@ next = document.getElementById('next')
  
 
 upPage.addEventListener('click', ()=>{
-    if(product_name.value.length < 10 || product_description.value.length < 10){
-        userWarning("Nome do Produto ou serviço, devem ter mais de 10 caracteres...")
-    }else if(url_img_pr.value=="" && url_img1.value=="" && url_img2.value=="" && url_img3.value=="" && url_img4.value==""){
-        userWarning("Adicione pelo menos uma imagem ilustrativa, de seu produto ou serviço...")
-    }       
-    else
         if(selected > 0){ 
             previous = document.getElementById('previous') 
             next = document.getElementById('next')    
@@ -40,21 +26,23 @@ upPage.addEventListener('click', ()=>{
 })
 
 downPage.addEventListener('click', ()=>{
-   if(selected < 2){
-    next = document.getElementById('next')     
-    previous = document.getElementById('previous') 
   
-    next.removeAttribute('id')
-    previous.removeAttribute('id')
-     
-     
-        console.log(selected)
-        sections[selected].setAttribute('id','previous')
-            selected++        
-            sections[selected].setAttribute('id', 'next')
+        if(selected < 2){
+            next = document.getElementById('next')     
+            previous = document.getElementById('previous') 
         
-   
-   }
+            next.removeAttribute('id')
+            previous.removeAttribute('id')
+            
+            
+                console.log(selected)
+                sections[selected].setAttribute('id','previous')
+                    selected++        
+                    sections[selected].setAttribute('id', 'next')
+                
+        
+        }
+ 
 })
 
 function userWarning(mensagem){
